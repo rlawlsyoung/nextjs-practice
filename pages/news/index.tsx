dce5dc15d9e03a3d4c71ba1b5156eadf571c4099
김진영 <jinyoung01099@gmail.com>
2023-02-02 18:00:42 +0900

    [Add] nextJS의 Link 컴포넌트 사용

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>NewsPage</h1>;
+  return (
+    <>
+      <h1>NewsPage</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs"> NextJS Is A Great Framework.</Link>
+        </li>
+        <li>
+          <Link href="/news/ramengurakbu">
+            Ramen Gurakbu Is A Great Restaurant.
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
