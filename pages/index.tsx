db3c31ceec580e88df424cbfae6f1e52a9f660ec
김진영 <jinyoung01099@gmail.com>
2023-02-17 19:37:45 +0900

    [Add] 유저 데이터 mockdata로 구현 및 출력

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,100 @@
-const HomePage = () => {
-  return <h1>HomePage</h1>;
+interface userdataType {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: any;
+}
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+];
+
+const HomePage = (props: any) => {
+  return (
+    <>
+      <h1>User List</h1>
+      <ul>
+        {DUMMY_DATA.map((user: userdataType) => {
+          return (
+            <li>
+              <title>
+                {user.name} ({user.username})
+              </title>
+              <p>email : {user.email}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: 1,
+    },
+    revalidate: 1,
+  };
+}
 
 export default HomePage;
